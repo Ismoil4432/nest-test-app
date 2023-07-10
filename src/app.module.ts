413873e049de901613a279fd1cea58cd3b16a930
Ismoil4432 <ismoil4432@gmail.com>
2023-07-10 20:34:40 +0500

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,18 @@ import { User } from './user/model/user.model';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.PG_HOST,
-      port: Number(process.env.PG_PORT) || 5432,
-      username: process.env.PG_USER,
-      password: String(process.env.PG_PASSWORD),
-      database: process.env.PG_DB,
-      autoLoadModels: true,
-      logging: false,
-      models: [User],
-    }),
-    UserModule,
+    // SequelizeModule.forRoot({
+    //   dialect: 'postgres',
+    //   host: process.env.PG_HOST,
+    //   port: Number(process.env.PG_PORT) || 5432,
+    //   username: process.env.PG_USER,
+    //   password: String(process.env.PG_PASSWORD),
+    //   database: process.env.PG_DB,
+    //   autoLoadModels: true,
+    //   logging: false,
+    //   models: [User],
+    // }),
+    // UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
